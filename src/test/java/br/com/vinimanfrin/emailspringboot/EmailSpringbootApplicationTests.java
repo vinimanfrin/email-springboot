0162fcfcecd99dd043fa5714c94997ab0168b2ed
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-01-17 10:25:43 -0300

    Criação e configurações inicias da api

---
+++ src/test/java/br/com/vinimanfrin/emailspringboot/EmailSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinimanfrin.emailspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
